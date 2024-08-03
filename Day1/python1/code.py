acfacdfbb87522f882e0e140b199a068fe3a3c55
Seema Saharan <48756444+seema1711@users.noreply.github.com>
2024-08-03 12:14:30 +0000

    Day 1

---
+++ Day1/python1/code.py
@@ -0,0 +1,12 @@
+# num = input().split()
+# print(num, type(num))
+
+# a, b, c = map(float, input().split())
+# print(a)
+# print(b)
+# print(c)
+
+'''Convert to List'''
+lst = list(map(int, input().split()))
+print(lst)
+print("Lenght of lst is ", len(lst))
---
+++ Day1/python1/data_type.py
@@ -0,0 +1,27 @@
+name = "Seema"
+age = 25
+weight = 50.5
+gender = "F"
+isEngineer = True
+fruitsILike = {'Kiwi', 'Strawberry', 'Mango'}
+earningFrom2024 = [10, 12, 12, 15]
+earningBefore2024 = (7, 8, 9)
+
+toBuy = {
+    "orange": 4,
+    "mango": 3,
+    "kiwi": 1
+}
+overHyped = None
+
+print(name, type(name))
+print(age, type(age))
+print(weight, type(weight))
+print(gender, type(gender))
+print(isEngineer, type(isEngineer))
+print(fruitsILike, type(fruitsILike))
+print(earningFrom2024, type(earningFrom2024))
+print(earningBefore2024, type(earningBefore2024))
+print(toBuy, type(toBuy))
+print(overHyped, type(overHyped))
+
